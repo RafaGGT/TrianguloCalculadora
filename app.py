db7050ec94950ae69f435818dc0ea7457e960843
Rafael <rg.gallegostapia@gmail.com>
2025-11-18 23:11:31 -0300

    nuevas funciones y sin controller

--- app.py
+++ app.py
@@ -1,15 +1,4 @@
 from vista.menu import Menu
-from modelo.Triangulo import Triangulo
-from controlador.TrianguloController import Controller
 
-
-
-vista = Menu()
-modelo = Triangulo()
-controlador = Controller(vista, modelo)
-
-# EL MENU PIDE DATOS
-lado_opuesto, angulo_opuesto, angulo_conocido = vista.mostrarMenu()
-
-# EL CONTROLADOR CALCULA
-controlador.calcular_lado_seno(lado_opuesto, angulo_opuesto, angulo_conocido)
+menu = Menu()
+menu.mostrarMenu()
--- vista/menu.py
+++ vista/menu.py
@@ -1,3 +1,4 @@
+from modelo.Triangulo import Triangulo as tri
 import os 
 class Menu:
 
@@ -8,11 +9,13 @@ class Menu:
         print("3. Calcular angulo usando el Teorema del Coseno")
         print("4. Calcular lado usando el Teorema del Coseno")
         print("5. Salir")
-        return self.obtenerOpcion()   # <-- AQUÍ ESTÁ EL ARREGLO
+        return self.obtenerOpcion()   
 
 
     def obtenerOpcion(self):
-        funciones = {"1": self.ladoSeno}
+        funciones = {
+            "1": self.ladoSeno, 
+            "2": self.anguloSeno}
         opcion = input("Seleccione una opción (1-5): ")
         if opcion in funciones:
             return funciones[opcion]()
@@ -24,4 +27,18 @@ class Menu:
         lado_opuesto = float(input("Ingrese el lado opuesto conocido: "))
         angulo_opuesto = float(input("Ingrese el ángulo opuesto conocido (en grados): "))
         angulo_conocido = float(input("Ingrese el ángulo conocido (en grados): "))
-        return lado_opuesto, angulo_opuesto, angulo_conocido
+        resultado = tri().teorema_seno_lado(lado_opuesto, angulo_opuesto, angulo_conocido)
+        print("\nTu ecuación:")
+        print(f"x / sin({angulo_conocido}°) = {lado_opuesto} / sin({angulo_opuesto}°)\n")
+        print(f"Resultado: {resultado}\n")
+        return self.obtenerOpcion()
+
+    def anguloSeno(self):
+        lado_opuesto = float(input("Ingrese el lado opuesto conocido: "))
+        angulo_opuesto = float(input("Ingrese el ángulo opuesto conocido (en grados): "))
+        lado_conocido = float(input("Ingrese el lado conocido: "))
+        resultado = tri().teorema_seno_angulo(lado_opuesto, angulo_opuesto, lado_conocido)
+        print("\nTu ecuación:")
+        print(f" {lado_conocido} / sin(x°) = sin({angulo_opuesto}°) / {lado_opuesto}\n")
+        print(f"Resultado: {resultado}\n")
+        return self.obtenerOpcion()
